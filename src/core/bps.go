a91b0ab1136e7ee638a21a28b25334538d4ab9e6
Jesus Alvarez <jeezusjr@gmail.com>
2015-12-29 00:11:34 -0800

    core/bps.go: Final cleanup

--- src/core/bps.go
+++ src/core/bps.go
@@ -3,7 +3,7 @@ package core
 import "time"
 
 // Maximum number of points used to calculate the average
-var totalPoints = 10
+var windowSize = 10
 
 type progressPoint struct {
 	time              time.Time
@@ -13,6 +13,7 @@ type progressPoint struct {
 type bytesPerSecond struct {
 	timeStart time.Time
 	points    []progressPoint
+	counter   uint64 // Used to track bytes that are added in between seconds
 }
 
 func newBytesPerSecond() *bytesPerSecond {
@@ -27,14 +28,11 @@ func (b *bytesPerSecond) addPoint(totalBytesWritten uint64) {
 		addPoint = true
 	}
 	if addPoint {
-		b.points = append(b.points, progressPoint{
-			time:              time.Now(),
-			totalBytesWritten: totalBytesWritten,
-		})
-		if len(b.points) > totalPoints {
-			end := len(b.points)
-			b.points = b.points[end-totalPoints : end]
-		}
+		b.counter += totalBytesWritten
+		b.points = append(b.points, progressPoint{time: time.Now(), totalBytesWritten: b.counter})
+		b.counter = 0
+	} else {
+		b.counter += totalBytesWritten
 	}
 }
 
@@ -47,8 +45,24 @@ func (b *bytesPerSecond) calc() uint64 {
 	if len(b.points) == 0 {
 		return 0
 	}
+	points := b.points
+	end := len(b.points)
+	if end > windowSize {
+		points = b.points[end-windowSize : end]
+	}
+	for _, y := range points {
+		tBytes += y.totalBytesWritten
+	}
+	return uint64(float64(tBytes / uint64(len(points))))
+}
+
+func (b *bytesPerSecond) calcFull() uint64 {
+	var tBytes uint64
+	if len(b.points) == 0 {
+		return 0
+	}
 	for _, y := range b.points {
 		tBytes += y.totalBytesWritten
 	}
-	return uint64(float64(tBytes / uint64(len(b.points))))
+	return uint64(float64(tBytes/uint64(len(b.points))) / time.Since(b.timeStart).Seconds())
 }
